d2643f2644c5ca1e1c01dfc8f1779e91d31cc4b3
Sophie Bonneval <maldorana@gmail.com>
2021-04-26 13:12:26 +0100

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
     def new
         @user = User.new
     end
@@ -12,6 +14,22 @@ class UsersController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(whitelist)
+            flash[:notice] = "Your account information was updated successfully"
+            redirect_to root_path
+        else
+            render 'edit'
+        end
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
     def whitelist
