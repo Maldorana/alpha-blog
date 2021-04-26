23d0e304477f4f48cc609fa2b6befd63fcac2c5d
Sophie Bonneval <maldorana@gmail.com>
2021-04-26 12:38:22 +0100

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(whitelist)
+        if @user.save
+            flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}. Your account has been successfully created."
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    def whitelist
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
