efb4b0bca1ba855c9e95225a2a1a94e01f2a2230
Sophie Bonneval <maldorana@gmail.com>
2021-04-28 12:48:44 +0100

    Restrict actions from controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)    end
@@ -35,12 +37,21 @@ class UsersController < ApplicationController
         end
     end
 
+    private
+
     def set_user
         @user = User.find(params[:id])
     end
 
     def whitelist
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "You can only edit or delete your own profile"
+            redirect_to @user
+        end
     end
 
 end
