fd68d19e9cc7756cd619228abb5b47bf8298c8a1
Sophie Bonneval <maldorana@gmail.com>
2021-04-29 16:40:07 +0100

    Create category & test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,17 @@
 class CategoriesController < ApplicationController
 
     def new
+        @category = Category.new
+    end
 
+    def create
+        @category = Category.new(whitelist)
+        if @category.save
+            flash[:notice] = "Category was created successfully"
+            redirect_to @category
+        else
+            render 'new'
+        end
     end
 
     def show
@@ -9,7 +19,13 @@ class CategoriesController < ApplicationController
     end
 
     def index
-        
+
+    end
+
+    private
+
+    def whitelist
+        params.require(:category).permit(:name)
     end
 
 end
