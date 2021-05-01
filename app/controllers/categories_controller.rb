b2610a2881963bc858d3827257f2f9b0d9ed9b46
Sophie Bonneval <maldorana@gmail.com>
2021-05-01 11:58:10 +0100

    Category index and show pages finished + edit action added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
     before_action :require_admin, except: [:show, :index]
 
     def new
@@ -16,14 +17,30 @@ class CategoriesController < ApplicationController
     end
 
     def show
-        @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)    
     end
 
     def index
-        @categories = Category.paginate(page: params[:page], per_page: 10)
+        @categories = Category.paginate(page: params[:page], per_page: 5)
+    end
+
+    def edit
+    end
+
+    def update
+        if @category.update(whitelist)
+            flash[:notice] = "Category name was updated successfully"
+            redirect_to @category
+        else
+            render 'edit'
+        end
     end
 
     private
+
+    def set_article
+        @category = Category.find(params[:id])
+    end
 
     def whitelist
         params.require(:category).permit(:name)
