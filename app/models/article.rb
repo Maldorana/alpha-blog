8b828fc21680040a21b51d31742e067990e1a2a2
Maldorana <maldorana@gmail.com>
2021-11-13 14:42:05 +0000

    Update images homepage

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
     has_many :article_categories
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: { minimum: 4, maximum: 100 }
-    validates :description, presence: true, length: { minimum: 6, maximum: 100000 }
+    validates :description, presence: true, length: { minimum: 6, maximum: 1000000 }
 end
